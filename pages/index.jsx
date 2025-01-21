3c18d1ab5d43f2ded893952a7967fbbbc9709e72
Dmitriy <healler00@mail.ru>
2025-01-21 10:45:49 +0300

    add player move timer

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,17 +1,34 @@
 import { useState } from "react";
-import { GameField, GameInfo, GameTitle } from "../components/game";
+import {
+  GameField,
+  GameInfo,
+  GameTitle,
+  useGameState,
+} from "../components/game";
 import { Header } from "../components/header";
 
 export default function HomePage() {
-  const [playersCount] = useState(2);
+  const [playersCount] = useState(4);
+  const { cells, currentMove, nextMove, handleCellClick } =
+    useGameState(playersCount);
 
   return (
     <div className="bg-slate-50 min-h-screen">
       <Header />
       <main className="pt-6 mx-auto w-max">
         <GameTitle playersCount={playersCount} />
-        <GameInfo playersCount={playersCount} className="mt-4" />
-        <GameField playersCount={playersCount} className="mt-6" />
+        <GameInfo
+          className="mt-4"
+          playersCount={playersCount}
+          currentMove={currentMove}
+        />
+        <GameField
+          className="mt-6"
+          cells={cells}
+          currentMove={currentMove}
+          nextMove={nextMove}
+          handleCellClick={handleCellClick}
+        />
       </main>
     </div>
   );
